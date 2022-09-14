d32beec7950b958afddfbb8878dd405f64d9afee
stefanoaragoni <69864405+stefanoaragoni@users.noreply.github.com>
2022-09-14 14:54:07 -0600

    get cantidad estados DONE

--- thompson.py
+++ thompson.py
@@ -5,12 +5,13 @@ class Thompson:
 
         self.simbolos = []
         self.estados = []
-        self.inicio = []
+        self.inicio = ["0"]
         self.aceptacion = []
         self.transiciones = []
 
         self.getSimbolos()
         self.getEstados()
+        self.Thompson()
     
     ##################### FUNCIONES #######################
 
@@ -22,5 +23,39 @@ class Thompson:
                 self.simbolos.append(i)
 
     def getEstados(self):
-        pass
+        list = [x for x in self.infix]
+        contadorEstados = 0
+        last = ''
 
+        for i in list:
+            if i.isalpha():
+                contadorEstados += 2
+
+                if last.isalpha():
+                    contadorEstados -= 1
+
+            elif i == '*':
+                contadorEstados += 2
+            
+            elif i == '|':
+                contadorEstados += 2
+
+            elif i == '?':
+                contadorEstados += 4
+            
+            last = i
+
+        for i in range(contadorEstados):
+            self.estados.append(str(i))
+
+    def Thompson(self):
+        self.postfix = "ab.c|#."
+
+        for i in self.postfix:
+            if i.isalpha():
+                pass
+
+t = Thompson('(ab)|c')
+print("SIMBOLOS:",t.simbolos)
+print("ESTADOS:",t.estados)
+print("INICIO:",t.inicio)
