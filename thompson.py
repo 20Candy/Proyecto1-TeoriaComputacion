9b9c0351a3d2a52558a240d7c14e157d02317b06
stefanoaragoni <69864405+stefanoaragoni@users.noreply.github.com>
2022-09-14 16:17:24 -0600

    WIP concat

--- thompson.py
+++ thompson.py
@@ -1,3 +1,6 @@
+import copy
+from turtle import pos
+
 class Thompson:
     def __init__(self, infix):
         self.infix = infix
@@ -11,7 +14,12 @@ class Thompson:
 
         self.getSimbolos()
         self.getEstados()
-        self.Thompson()
+
+        self.estadosCopy = copy.deepcopy(self.estados)
+
+        self.postfix = "ab.c*."
+        self.postfix = [x for x in self.postfix]
+        self.Thompson(self.postfix)
     
     ##################### FUNCIONES #######################
 
@@ -48,14 +56,89 @@ class Thompson:
         for i in range(contadorEstados):
             self.estados.append(str(i))
 
-    def Thompson(self):
-        self.postfix = "ab.c|#."
+    def Thompson(self, postfix):
+        #ab.c|
+        tempTransicion = []
+        temp2Transicion = []
+        i = ""
 
-        for i in self.postfix:
-            if i.isalpha():
-                pass
+        try:
+            i = postfix[-1]
+        except:
+            return
 
-t = Thompson('(ab)|c')
-print("SIMBOLOS:",t.simbolos)
-print("ESTADOS:",t.estados)
-print("INICIO:",t.inicio)
+        if i.isalpha():
+            temp = self.estadosCopy.pop(0)
+            self.aceptacion.append(temp)
+
+            tempTransicion.append(temp)
+            tempTransicion.append(i)
+            tempTransicion.append(self.estadosCopy[0])
+
+            self.transiciones.append(tempTransicion)
+            tempTransicion = []
+
+        elif i == '.':
+            if(postfix[-2].isalpha() and postfix[-3].isalpha()):
+                postfix.pop()
+                one = postfix.pop()
+                two = postfix.pop()
+                three = copy.deepcopy(postfix)
+
+                self.Thompson(three)
+                self.Thompson(two)
+                self.Thompson(one)
+
+            elif(postfix[-2].isalpha() and (postfix[-3].isalpha() == False)):
+                postfix.pop()
+                one = postfix.pop()
+                two = []
+                three = []
+
+                flag = False
+                for i in reversed(postfix):
+                    if(flag):
+                        three.append(postfix[i])
+                    else:
+                        three.append(postfix[i])
+
+                self.Thompson(two)
+                self.Thompson(one)
+            
+            elif((postfix[-2].isalpha() == False)):
+                postfix.pop()
+                if("." in postfix):
+                    self.Thompson(postfix)
+
+                else:
+                    one = postfix.pop()
+                    two = copy.deepcopy(postfix)
+
+                    self.Thompson(two)
+                    self.Thompson(one)
+
+
+        elif i == '*':
+            postfix.pop()
+            one = postfix.pop()
+            two = copy.deepcopy(postfix)
+
+            
+
+            self.Thompson(two)
+            self.Thompson(one)
+        
+        elif i == '|':
+            pass
+
+        elif i == '?':
+            pass
+
+
+t = Thompson('ab.c.')
+print("\nEXPRESION REGULAR:", t.infix)
+print("\nSIMBOLOS: [",', '.join(t.simbolos),"]")
+print("ESTADOS: [",', '.join(t.estados),"]")
+print("INICIO: [",', '.join(t.inicio),"]")
+print("TRANSICIONES:", t.transiciones)
+print("\n")
